fa9d99b20bbf336afb0a4cbd5f38148ce06b70ae
Sudarsan-ks <sansudar908@gmail.com>
2024-07-07 12:35:31 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-export const auth = (req, res, next) =>{
-    try{
-        const token = req.header("Authorization")
-        jwt.verify(token, process.env.SECRET_KEY);
-        next()
-    }
-    catch(error){
-        res.send({error: error.message})
-    }
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    jwt.verify(token, process.env.SECRET_KEY);
+    next();
+  } catch (error) {
+    res.send({ error: error.message });
+  }
+};
 
-}
+module.exports = auth;
