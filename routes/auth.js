e6eb1a959dda76502f4d66725ce858cd51bd598d
Sudarsan-ks <sansudar908@gmail.com>
2024-07-07 13:07:43 +0530

    update

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
+    const token = req.header('Authorization');
     if (!token) {
       return res.status(401).send({ error: 'No token provided' });
     }
